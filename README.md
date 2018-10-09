efb71046a04210ad021e43f7b29ea3fc189cf5d7
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-10-08 23:20:49 -0300

    First version upload

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# mirror-check
+# Mirror Check
 Check if mirrors are updated and generate a status web page.
+
+## Installation
+
+### Prerequisites
+* lftp
+* Python 3
+* Python Pip
+* Jinja2
+* ruamel.yaml
+
+### Ubuntu installation
+You can easy install all the prerequisites on Ubuntu with the following lines:
+```sh
+sudo apt-get install lftp
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install Jinja2
+sudo pip3 install ruamel.yaml
+```
+
+### Configuration
+Edit the ```config.yaml``` file. You can replace the example names used and add more mirrors.
+If you have a new template, edit the ```template``` name, that corresponds to the folder name inside ```/templates``` folder.
+
+Edit the ```/conf``` folder files with the settings of each ```lftp``` connection. The ```.conf``` filename relates to the used in ```config.yaml``` file.
+
+## Running
+To generate the status web page, you can setup a ```cronjob``` with this command:
+```sh
+cd /mirror-check ; python3 generate.py > webpage/index.html
+```
+Replace the ```mirror-check``` directory with your installation path. Put the files of ```/webpage``` folder in a web server directory, or just create a symbolic link between them.
+
+## Template
+If you need modify the web page style, edit the ```template.j2``` file in the chosen template folder.
---
+++ generate.py
@@ -0,0 +1,68 @@
+from ruamel import yaml
+from jinja2 import Environment, FileSystemLoader, Template
+import datetime
+import os
+
+# Edit ls output
+def txt_editor(file_in, file_out):
+    # Open in and out files
+    f = open(file_in, "r")
+    g = open(file_out, "w")
+
+    # Get line by line
+    for line in f:
+        # If line not empty
+        if line.strip():
+            # If isnt a directory
+            if not line.startswith('d'):
+                # Write only last 3 columns in output
+                g.write("\t".join(line.split()[2:]) + "\n")
+
+    # Close files
+    f.close()
+    g.close()
+
+
+now = datetime.datetime.now()
+
+file = open('./config.yaml', 'r')
+config_data = yaml.load(file, Loader=yaml.Loader)
+
+# # List main server
+os.system('lftp -c "source conf/original.conf && find -l . && exit" > lists/original.txt')
+txt_editor("lists/original.txt", "lists/original_list.txt")
+
+# Verify mirrors diff and update config data
+mirrors = config_data['mirrors']
+for mirror in mirrors:
+
+    os.system('lftp -c "source conf/' + mirror + '.conf && find -l . && exit" > lists/'  + mirror + '.txt')
+    txt_editor("lists/" + mirror + ".txt", "lists/" + mirror + "_list.txt")
+
+    os.system("diff -u lists/original_list.txt lists/" + mirror + "_list.txt > webpage/" + mirrors[mirror]['diffFile'])
+
+    if os.stat(mirrors[mirror]['diffFile']).st_size==0:
+        if mirrors[mirror]['updated'] == False:
+            mirrors[mirror]['lastUpdate'] = now.strftime("%Y-%m-%d %H:%M")
+        mirrors[mirror]['updated'] = True
+    else:
+        mirrors[mirror]['updated'] = False
+
+# Update time
+config_data['lastRun'] = now.strftime("%Y-%m-%d %H:%M")
+template = config_data['template']
+
+# Save data
+file = open('./config.yaml', 'w')
+yaml.dump(config_data, file, Dumper=yaml.RoundTripDumper)
+
+# Load Jinja2 template
+env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+template = env.get_template('templates/' + template +'/template.j2')
+
+# Update template files in the webpage folder
+os.system('cp templates/' + template +'/* webpage/ ; rm webpage/template.j2')
+
+# Render the template with data and save in webpage/index.html
+file = open('webpage/index.html', 'w')
+file.write(template.render(config_data))
